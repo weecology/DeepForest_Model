90443553143b2670a927f20cb385ea3814632653
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-02-26 20:28:21 -0800

    new trainer method

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ comet_logger = CometLogger(api_key="ypQZhYfs3nSyKzOfz13iuJpj2",
 time.sleep(random.randint(0,10))
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 comet_logger.experiment.log_parameter("timestamp", timestamp)
-savedir = "{}/{}".format("/orange/ewhite/b.weinstein/NeonTreeEvaluation/snapshots/",timestamp)
+savedir = "{}/{}".format("/orange/ewhite/b.weinstein/retinanet/",timestamp)
 
 try:
     os.mkdir(savedir)
@@ -27,13 +27,13 @@ except:
 #Create objects
 m = main.deepforest()
 im_callback = images_callback(csv_file=m.config["validation"]["csv_file"], root_dir=m.config["validation"]["root_dir"], savedir=savedir, n=10)
-m.create_trainer(callbacks=im_callback, logger=comet_logger)
+m.create_trainer(callbacks=[im_callback], logger=comet_logger)
 
 comet_logger.experiment.log_parameters(m.config)
 comet_logger.experiment.log_parameters(m.config["train"])
 comet_logger.experiment.log_parameters(m.config["validation"])
 
-m.run_train()
+m.trainer.fit(m)
 m.evaluate(csv_file=m.config["validation"]["csv_file"], root_dir=m.config["validation"]["root_dir"])
 
-torch.save(m.model.state_dict(), "{}/hand_annotated_model.pt".format(savedir))
+m.save_checkpoint()
