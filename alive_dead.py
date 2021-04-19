7828660121c1626f0ad6a57a43144d9a6d0e5057
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-04-19 15:53:32 -0700

    first pass at alive dead model

---
+++ alive_dead.py
@@ -0,0 +1,68 @@
+#Alive Dead Model, optionally building from release tree crown model
+import comet_ml
+import glob
+import time
+import random
+from datetime import datetime
+import os
+from deepforest import main
+from pytorch_lightning.loggers import CometLogger
+
+def train(train_path, test_path, pretrained=False, image_dir = "/orange/idtrees-collab/NeonTreeEvaluation/evaluation/RGB/", debug=False, savedir="/orange/idtrees-collab/DeepTreeAttention/Dead/"):
+    
+    comet_logger = CometLogger(api_key="ypQZhYfs3nSyKzOfz13iuJpj2",
+                                  project_name="deepforest-pytorch", workspace="bw4sz")
+    comet_logger.experiment.add_tag("DeadAlive")
+    
+    #add small sleep for SLURM jobs
+    time.sleep(random.randint(0,10))
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    comet_logger.experiment.log_parameter("timestamp", timestamp)
+    savedir = "{}/{}".format(savedir,timestamp)
+    
+    try:
+        os.mkdir(savedir)
+    except:
+        pass
+    
+    m = main.deepforest(num_classes=2, label_dict={"Alive":0,"Dead":1})
+    m.config["train"]["csv_file"] = train_path
+    m.config["train"]["root_dir"] = image_dir
+    m.config["validation"]["csv_file"] = test_path
+    m.config["validation"]["root_dir"] = image_dir
+    
+    if debug:
+        m.config["train"]["fast_dev_run"] = True
+        m.config["gpus"] = None
+        m.config["workers"] = 0
+    
+    m.create_trainer()
+    
+    m.trainer.fit(m)
+    
+    result_dict = m.evaluate(csv_file=m.config["validation"]["csv_file"], root_dir=m.config["validation"]["root_dir"])
+    
+    comet_logger.experiment.log_metric("test_box_precision",result_dict["box_precision"])
+    comet_logger.experiment.log_metric("test_box_recall",result_dict["box_recall"])
+    
+    result_dict["class_recall"].to_csv("{}/class_recall.csv".format(savedir))
+    comet_logger.experiment.log_asset("{}/class_recall.csv".format(savedir))
+    
+    result_dict["results"].to_csv("{}/results.csv".format(savedir))
+    comet_logger.experiment.log_asset("{}/results.csv".format(savedir))
+    
+    boxes = m.predict_file(csv_file=m.config["validation"]["csv_file"], root_dir=m.config["validation"]["root_dir"], savedir=savedir)
+    images = glob.glob("{}/*.png".format(savedir))
+    random.shuffle(images)
+    for img in images[:20]:
+        comet_logger.experiment.log_image(img)
+    boxes.to_csv("{}/benchmark_predictions.csv".format(savedir))
+    comet_logger.experiment.log_asset("{}/benchmark_predictions.csv".format(savedir))
+    
+    m.save_model("{}/hand_annotated.pl".format(savedir))
+    comet_logger.experiment.log_parameter("saved model", "{}/hand_annotated.pl".format(savedir))
+
+             
+if __name__ == "__main__":
+    train(train_path="/orange/idtrees-collab/DeepTreeAttention/data/dead_train.csv",
+          test_path="/orange/idtrees-collab/DeepTreeAttention/data/dead_train.csv")
