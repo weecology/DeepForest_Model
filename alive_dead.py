8ce4d1e52119a88fca8d2f7d561f6806621edade
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-04-22 09:46:52 -0700

    Add logger

--- alive_dead.py
+++ alive_dead.py
@@ -8,13 +8,31 @@ import os
 from deepforest import main
 from pytorch_lightning.loggers import CometLogger
 from TwoHeadedRetinanet import TwoHeadedRetinanet
+
+
+#Overwrite default training log
+class alive_dead_module(main.deepforest):
+    def training_step(self, batch, batch_idx):
+        """Train on a loaded dataset
+        """
+        path, images, targets = batch
     
+        loss_dict = self.model.forward(images, targets)
+    
+        # sum of regression and classification loss
+        losses = sum([loss for loss in loss_dict.values()])
+        # Log loss
+        for key, value in loss_dict.items():
+            self.log("train_{}".format(key), value, on_epoch=True)
+            
+        return losses
+
 def train(train_path, test_path, pretrained=False, image_dir = "/orange/idtrees-collab/NeonTreeEvaluation/evaluation/RGB/", debug=False, savedir="/orange/idtrees-collab/DeepTreeAttention/Dead/"):
     
     comet_logger = CometLogger(api_key="ypQZhYfs3nSyKzOfz13iuJpj2",
                                   project_name="deepforest-pytorch", workspace="bw4sz")
     comet_logger.experiment.add_tag("DeadAlive")
-    comet_logger.experiment.add_tag("No pretraining")
+    
     
     #add small sleep for SLURM jobs
     time.sleep(random.randint(0,10))
@@ -28,11 +46,10 @@ def train(train_path, test_path, pretrained=False, image_dir = "/orange/idtrees-
         pass
     
     #Get release state dict
-    m = main.deepforest()
+    m = alive_dead_module()
     m.use_release()
     
     #Overwrite original retinanet with a two headed task
-    
     m.model = TwoHeadedRetinanet(trained_model=m.model, num_classes_task2=2, freeze_original=True)
     m.label_dict = {"Alive":0,"Dead":1}
     #update the labels for the new task
@@ -47,8 +64,13 @@ def train(train_path, test_path, pretrained=False, image_dir = "/orange/idtrees-
         m.config["gpus"] = None
         m.config["workers"] = 0
         m.config["distributed_backend"] = None
+        m.config["batch_size"] = 2
     
-    m.create_trainer()
+    comet_logger.experiment.log_parameters(m.config)
+    comet_logger.experiment.log_parameters(m.config["train"])
+    comet_logger.experiment.log_parameters(m.config["validation"])
+    
+    m.create_trainer(logger=comet_logger)
     
     m.trainer.fit(m)
     
