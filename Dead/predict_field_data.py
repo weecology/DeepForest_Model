c4c4de3840fbf531c0b74a245855b9e677f352db
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2021-04-30 06:38:19 -0700

    figures

--- Dead/predict_field_data.py
+++ Dead/predict_field_data.py
@@ -251,3 +251,16 @@ def run(checkpoint_path, image_dir, savedir, field_path, num_workers=10, canopy_
         data=results,
         x="plantStatus", hue="Dead", palette="dark")
     plt.savefig("Dead/figures/plantStatus.png")
+    
+    return results
+
+if __name__ == "__main__":
+    results = run(
+        checkpoint_path="/orange/idtrees-collab/DeepTreeAttention/Dead/cef3e91d8a9c4e848d85d333233b3c7f.pl",
+        image_dir="/orange/idtrees-collab/NeonTreeEvaluation/evaluation/RGB",
+        savedir="Dead/figures/",
+        field_path="Dead/data/neon_vst_data_2021.csv",
+        num_workers=10,
+    debug=True)
+    results.to_csv("Dead/figures/results.csv")
+        
--- tests/test_predict_field_data.py
+++ tests/test_predict_field_data.py
@@ -7,11 +7,12 @@ from Dead import predict_field_data
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 def test_run(tmpdir):
-    predict_field_data.run(
+    results = predict_field_data.run(
         checkpoint_path="snapshots/alive_dead.pl",
         image_dir="/Users/benweinstein/Documents/NeonTreeEvaluation/evaluation/RGB",
         savedir=tmpdir,
         field_path="Dead/data/neon_vst_data_2021.csv",
         num_workers=0,
-    debug=True)
+    debug=False)
+    results.to_csv("Dead/figures/results.csv")
     
