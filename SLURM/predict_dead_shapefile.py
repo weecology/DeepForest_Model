d139d8e206fe7472a1e18ae4e9f96e265f6a3b6e
bw4sz <benweinstein2010@gmail.com>
2021-06-10 12:26:43 -0400

    merge

--- SLURM/predict_dead_shapefile.py
+++ SLURM/predict_dead_shapefile.py
@@ -4,9 +4,9 @@
 #SBATCH --mail-user=benweinstein2010@gmail.com  # Where to send mail
 #SBATCH --account=ewhite
 #SBATCH --nodes=1                 # Number of MPI ranks
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=3
 #SBATCH --mem=20GB
-#SBATCH --time=5:00:00       #Time limit hrs:min:sec
+#SBATCH --time=24:00:00       #Time limit hrs:min:sec
 #SBATCH --output=/home/b.weinstein/logs/DeepForest_Dead%j.out   # Standard output and error log
 #SBATCH --error=/home/b.weinstein/logs/DeepForest_Dead%j.err
 #SBATCH --partition=gpu
@@ -18,4 +18,4 @@ cd /home/b.weinstein/DeepForest_Model/Dead
 
 #comet debug
 #python alive_dead.py
-python predict.py
+python -m cProfile -o predict.prof predict.py
